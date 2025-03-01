ce565a6dcd04ff6b807eec89d0ff7075445b8d1d
Naoyuki Koyanagi <nba20093089@gmail.com>
2025-03-02 01:29:44 +0900

    プロフィールページ出品商品表示

--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -10,25 +10,32 @@
 <div class="profile-container">
     <div class="profile-header">
         <div class="profile-info">
-            <div class="avatar"></div>
+            <!-- プロフィール画像の表示 -->
+            <div class="avatar">
+                <img src="{{ Auth::user()->profile_image ? Storage::url(Auth::user()->profile_image) : asset('images/default-avatar.png') }}" alt="プロフィール画像">
+            </div>
+            <!-- ユーザー名の表示 -->
             <h1 class="username">{{ Auth::user()->name }}</h1>
         </div>
         <a href="{{ route('user.edit') }}" class="edit-button">プロフィールを編集</a>
     </div>
 
     <nav class="tabs">
-        <a href="#" class="tab active">出品した商品</a>
-        <a href="#" class="tab">購入した商品</a>
+        <a href="{{ route('user.sellList') }}" class="tab {{ request()->is('mypage?tab=sell') ? 'active' : '' }}">出品した商品</a>
+        <a href="{{ route('user.buyList') }}" class="tab {{ request()->is('mypage?tab=buy') ? 'active' : '' }}">購入した商品</a>
     </nav>
 
-    <div class="products-grid">
+    <div class="product-grid">
         @foreach ($items as $item)
-        <a href="{{ route('products.show', $product->id) }}" class="product-card">
-            <div class="product-image">
-                <img src="{{ asset('storage/products/' . $product->image) }}" alt="{{ $product->name }}">
-            </div>
-            <div class="product-name">{{ $product->name }}</div>
-        </a>
+        <div class="product-item">
+            <a href="{{ route('items.show', $item->id) }}">
+                <img src="{{ filter_var($item->image, FILTER_VALIDATE_URL) ? $item->image : Storage::url($item->image) }}"
+                    class="product-image">
+                <div class="product-info">
+                    <span class="product-name">{{ $item->name }}</span>
+                </div>
+            </a>
+        </div>
         @endforeach
     </div>
 </div>
