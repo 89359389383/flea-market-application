ebbb31e9eea54a7bc3c5796e5ce1642c704aea30
Naoyuki Koyanagi <nba20093089@gmail.com>
2025-03-19 23:40:17 +0900

    リクエスト修正(Profile)

--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -28,7 +28,7 @@ class ProfileRequest extends FormRequest
             'name' => 'required|string|max:255',
             'postal_code' => 'required|string|regex:/^\d{3}-\d{4}$/',
             'address' => 'required|string|max:255',
-            'building' => 'nullable|string|max:255',
+            'building' => 'required|string|max:255',
         ];
     }
 
@@ -38,10 +38,11 @@ class ProfileRequest extends FormRequest
             'profile_image.image' => 'プロフィール画像は画像ファイルである必要があります',
             'profile_image.mimes' => 'プロフィール画像はJPEGまたはPNG形式である必要があります',
             'profile_image.max' => 'プロフィール画像は2MB以下である必要があります',
-            'name.required' => 'ユーザー名を入力してください',
+            'name.required' => 'お名前を入力してください',
             'postal_code.required' => '郵便番号を入力してください',
             'postal_code.regex' => '郵便番号は「XXX-XXXX」の形式で入力してください',
             'address.required' => '住所を入力してください',
+            'building.required' => '建物名を入力してください',
         ];
     }
 }
